a7e24d00677639b5a4dcc1fd848da36ff15c2d30
Undef125 <076bct038@ioepc.edu.np>
2022-04-23 07:03:35 +0545

    fuu

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,27 +2,9 @@ import React from "react";
 import { ProgressBar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
-import { AiFillHtml5 } from "react-icons/ai";
-import { DiCss3 } from "react-icons/di";
-import { IoLogoJavascript } from "react-icons/io";
-import { FaReact, FaBootstrap } from "react-icons/fa";
-import { SiRedux, SiTypescript, SiCplusplus } from "react-icons/si";
+import { SiRedux} from "react-icons/si";
 
 type Props = {};
-
-// interface myComp {
-//     iconn?: IconType;
-//     percentage: number;
-// }
-
-// function Build({ iconn, percentage }: myComp) {
-//     return (
-//         <>
-//             <iconn size={54} />
-//             <ProgressBar animated now={99} label={`${99}%`} />
-//         </>
-//     );
-// }
 
 export const Skills = (props: Props) => {
     return (
