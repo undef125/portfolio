af4122fb3890d7e9ef6ef3eb0aae2ab075bcfe78
undef125 <076bct038@ioepc.edu.np>
2022-05-15 16:46:25 +0545

    added particles

---
+++ src/components/ParticlesPage.tsx
@@ -0,0 +1,90 @@
+import Particles from 'react-tsparticles';
+
+interface Props {
+  particlesInit: any;
+  particlesLoaded: any;
+}
+
+export const ParticlesPage = (props: Props) => {
+  return (
+    <div>
+        <Particles className="particles"
+      id="tsparticles"
+      init={props.particlesInit}
+      loaded={props.particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#3d404b",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#7ef4dc",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: false,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: true,
+            speed: 3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 50,
+          },
+          opacity: {
+            value: 0.3,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: false,
+      }}
+    />
+    </div>
+  )
+}
