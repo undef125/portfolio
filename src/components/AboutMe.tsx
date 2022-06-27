673d976fb7c4ada466b0b5b5b9cda9a0008bd967
undef125 <076bct038@ioepc.edu.np>
2022-06-27 12:22:26 +0545

    styling changes

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,17 +2,24 @@ export const AboutMe = () => {
     return (
         <>
             <div className="aboutmecontainer">
-                <div className="textSection">
-                    <div className="hi">Hi, I'm</div>
-                    <div className="name">Kapil Kunwar</div>
-                    <p>
-                        I'm a frontend web developer with a strong work
-                        ethic and a passion for what I do.<br/>Currently a third year computer engineering student at TU IOE, Purwanchal Campus Dharan.
-                    </p>
-                    <button className="cvButton">Download CV</button>
+                <div className="leftSection">
+                    <div className="textSection">
+                        <div className="hi">Hi, I'm</div>
+                        <div className="name">Kapil Kunwar</div>
+                        <p>
+                            I'm a frontend web developer with a strong work
+                            ethic and a passion for what I do.
+                            <br />
+                            Currently a third year computer engineering student
+                            at TU IOE, Purwanchal Campus Dharan.
+                        </p>
+                        <button className="cvButton">Download CV</button>
+                    </div>
                 </div>
-                <div className="imageSection">
-                    <img src="/prof.jpg" alt="" />
+                <div className="rightSection">
+                    <div className="imageSection">
+                        <img src="/prof.jpg" alt="" />
+                    </div>
                 </div>
             </div>
         </>
